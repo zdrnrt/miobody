e16fdd5d8e9d015d39d7636790f518ea7cef58bd
Задорин Артем <artfan95@gmail.com>
2026-02-24 23:29:01 +0700

    fix: Доработки структуры и вывод нового блока

--- src/entities/Chart/index.ts
+++ src/entities/Chart/index.ts
@@ -1,5 +1,5 @@
 import { getChart, getAll, uploadChart } from "./api/api";
 import { IChart, IChartSeries, IChartStat, IDataset } from "./model/model";
 
-export type {IChart, IChartSeries, IChartStat, IDataset}
+export type {IChart, IChartSeries, IChartStat, IDataset, }
 export {getChart, getAll, uploadChart}
